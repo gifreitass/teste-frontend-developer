65b3be5f7708ca92ac6eb47e3024e2aa08d83946
Giovanna de Freitas <giovannadefreitasaraujo@hotmail.com>
2026-04-16 17:21:27 -0300

    feat: create cta section, footer section and scroll animations

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+const elements = document.querySelectorAll('.fade-up');
+
+window.addEventListener('scroll', () => {
+  elements.forEach(el => {
+    const top = el.getBoundingClientRect().top;
+
+    if (top < window.innerHeight - 100) {
+      el.classList.add('active');
+    }
+  });
+});
