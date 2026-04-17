990641a1890513f3e990713297ebcfac8e10d022
Giovanna de Freitas <giovannadefreitasaraujo@hotmail.com>
2026-04-17 12:46:38 -0300

    feat: add responsive layout

--- src/js/main.js
+++ src/js/main.js
@@ -2,8 +2,10 @@ const elements = document.querySelectorAll('.fade-up');
 
 window.addEventListener('scroll', () => {
   elements.forEach(el => {
+    //pega a posição do elemento em relação à área visível da página
     const top = el.getBoundingClientRect().top;
 
+    //verifica se o elemento está visível e adiciona a classe para ativar a animação
     if (top < window.innerHeight - 100) {
       el.classList.add('active');
     }
